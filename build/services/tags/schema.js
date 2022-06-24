7c26988b6f5fb1d158041009ea506b2c34662717
Lidia Kovac <lidia.kovac1998@gmail.com>
2022-06-24 19:35:26 +0200

    removes build

--- build/services/tags/schema.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const tagSchema = new mongoose.Schema({
-    word: { type: String, required: true, unique: true },
-    color: { type: String, required: true },
-    overallOccurences: { type: Number, required: true },
-    yearlyOccurences: [{ year: String, occurences: Number }],
-});
-export default mongoose.model("Tag", tagSchema);
--- build/services/years/schema.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const yearSchema = new mongoose.Schema({
-    year: { type: String, required: true, unique: true },
-    tags: [{ type: mongoose.Types.ObjectId, ref: 'Tag' }],
-    poems: [{ type: mongoose.Types.ObjectId, ref: 'Poem' }]
-});
-export default mongoose.model("Year", yearSchema);
